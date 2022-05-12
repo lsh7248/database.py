5e9b5dc519f7dc9e8323411addd62182f9ff9557
10151032 <s-hyun.lee@kt.com>
2022-05-12 23:08:02 +0900

    oracle 11.2 연동테스트

---
+++ app/database.py
@@ -0,0 +1,23 @@
+import cx_Oracle
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "oracle+cx_oracle://admin:admin@localhost:49161/?service_name=xe&encoding=UTF-8&nencoding=UTF-8"
+# cx_Oracle.makedsn("localhost", 49161, sid="xe")
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    # connect_args={
+    #     "encoding": "UTF-8",
+    #     "nencoding": "UTF-8",
+    #     "mode": cx_Oracle.SYSDBA,
+    #     "events": True
+    # },
+    # max_identifier_length=30,
+    # connect_args={"check_same_thread": False}
+)
+print("DB Connections Success!")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
