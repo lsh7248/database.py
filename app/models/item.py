52d749e987cfe103832315bb6fd8e1352d84472e
10151032 <s-hyun.lee@kt.com>
2022-05-20 16:38:40 +0900

    FASTAPI 초기 구조 구축중..

--- app/models/item.py
+++ app/models/item.py
@@ -1,19 +1,7 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from .database import Base
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(20), unique=True, index=True)
-    hashed_password = Column(String(20))
-    is_active = Column(Boolean, default=True)
-
-    items = relationship("Item", back_populates="owner")
-
+from ..db.base_class import Base
 
 class Item(Base):
     __tablename__ = "items"
